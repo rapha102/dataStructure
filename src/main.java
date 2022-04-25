ad6eb0e25485370af15379aa90cf8e0b5ed7b260
Raphael Rumberg <raphaelrumberg.wsk@gmail.com>
2022-04-25 12:42:31 +0200

    created Binary tree with additional Node class and Tester

--- src/main.java
+++ src/main.java
@@ -1,8 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        createBinaryTree test = new createBinaryTree(3);
 
-        System.out.print(test.getter());
         SimpleAssoc sim1 = new SimpleAssoc();
 
         sim1.put("eins","512");
